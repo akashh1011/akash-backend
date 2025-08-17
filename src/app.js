bd75435ade5cfb95af5f3a4475b42765ae4cae19
Akash Prajapati <prajapatiaakash816@gmail.com>
2025-08-17 09:47:10 +0530

    Custom Api response and error handling done

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+const app=express()
+
+app.use(cors({
+  origin:process.env.CORS_ORIGIN,
+  credentials:true
+}))
+
+//middlewares
+
+//json data ko handle krne k liye lgaya h
+app.use(express.json({limit:"16kb"}))
+
+//jo bhi url routes hote h unko encode krne k liye 
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
+//files or images jo aate h unhe store krne k liye
+app.use(express.static("public"))
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
   path:'./env'
 })
 connectDB()
+.then(()=>{
+app.on("Error",(err)=>{
+  console.log("Error",err)
+})
+
+  app.listen(process.env.MONGO_URI || 9000,()=>{
+    console.log(`server is running on port: ${process.env.MONGO_URI}`)
+  })
+})
+.catch((err)=>{
+    console.log("MongoDB Connection failed",err)
+  })
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
