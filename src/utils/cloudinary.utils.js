f46ef43f3f7387fa9f43f96dde7c3115644509e3
Akash Prajapati <prajapatiaakash816@gmail.com>
2025-08-19 11:29:04 +0530

    MULTER AND FS DONE

---
+++ src/utils/cloudinary.utils.js
@@ -0,0 +1,31 @@
+//yhi file hme file ya other media upload krne degi 
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import * as fs from 'node:fs/promises';
+
+
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath)=>{
+  try {
+    if(!localFilePath) return null
+  const response = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+
+  // file has been successfuly uploaded
+  console.log("File has been successfulyy uploaded on cloudinary",response.url)
+  return response;
+}
+    
+  catch (error) {
+    fs.unlinkSync(localFilePath)
+    //remove the locally saved temporary file as the upload operation got failed
+    return null;
+    
+  }
+}
+
+  export {uploadOnCloudinary}
